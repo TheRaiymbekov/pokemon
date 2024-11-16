bffed46f2d21e95820ffdf8d0f1f52dff77c6f47
TheRaiymbekov <irajymbekov8@gmail.com>
2024-11-16 13:12:04 +0600

    pokemon

---
+++ pokemon/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import PokemonPage from "./page/PokemonPage";
+
+
+function App() {
+    return (
+        <div className="App">
+            <PokemonPage/>
+        </div>
+    );
+}
+
+
+export default App;
